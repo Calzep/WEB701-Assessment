c90d2ee00685e844633dfab0989533096a47568d
Calzep <caleb-easpn@nmit.live.ac.nz>
2025-08-18 16:42:20 +1200

    removed ids in favour of automatic ids

--- MExNPrototypes/server/models/ServicePurchase.js
+++ MExNPrototypes/server/models/ServicePurchase.js
@@ -1,11 +1,6 @@
 const mongoose = require('mongoose')
 
 const ServicePurchaseSchema = new mongoose.Schema({
-    id: {
-        type: String,
-        required: true,
-        unique: true
-    },
     service: {
         type: String,
         required: true,
--- MExNPrototypes/server/models/User.js
+++ MExNPrototypes/server/models/User.js
@@ -1,11 +1,6 @@
 const mongoose = require('mongoose')
 
 const UserSchema = new mongoose.Schema({
-    id: {
-        type: String,
-        required: true,
-        unique: true
-    },
     email: {
         type: String,
         required: true,
